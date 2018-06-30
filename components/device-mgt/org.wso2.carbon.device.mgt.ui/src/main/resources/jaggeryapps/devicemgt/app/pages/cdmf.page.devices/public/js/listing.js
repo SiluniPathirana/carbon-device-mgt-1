3806446c7e453610a1c4a1156ffa55b35578c2a4
SiluniPathirana <siluni@wso2.com>
2018-06-30 14:19:18 +0530

    Fix for allow username with apostrophe and space

--- components/device-mgt/org.wso2.carbon.device.mgt.ui/src/main/resources/jaggeryapps/devicemgt/app/pages/cdmf.page.devices/public/js/listing.js
+++ components/device-mgt/org.wso2.carbon.device.mgt.ui/src/main/resources/jaggeryapps/devicemgt/app/pages/cdmf.page.devices/public/js/listing.js
@@ -415,7 +415,7 @@ function loadDevices(searchType, searchParam) {
         $(row).attr('data-deviceid', htmlspecialchars(data.deviceIdentifier));
         $(row).attr('data-devicetype', htmlspecialchars(data.deviceType));
         $(row).attr('data-url', context + '/device/' + htmlspecialchars(data.deviceType) + '?id='
-                + htmlspecialchars(data.deviceIdentifier) + '&owner=' + cleanedOwner ;
+                + htmlspecialchars(data.deviceIdentifier) + '&owner=' + cleanedOwner );
         var model = htmlspecialchars(getPropertyValue(data.properties, 'DEVICE_MODEL'));
         var vendor = htmlspecialchars(getPropertyValue(data.properties, 'VENDOR'));
         var owner = cleanedOwner
